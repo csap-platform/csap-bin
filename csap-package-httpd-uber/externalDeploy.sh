ef05c4a3254568f91ffdf31502c52dfdd75dab42
peterdnight <p.eternightingale@gmail.com>
2022-01-10 12:42:59 -0500

    csap-master-merge 21.11

--- csap-package-httpd-uber/externalDeploy.sh
+++ csap-package-httpd-uber/externalDeploy.sh
@@ -5,7 +5,7 @@
 #
 # externalDeploy.sh 11.0.1 ~/stuff/myFile.tar.gz
 
-VERSION=${1:-2.4.51}
+VERSION=${1:-2.4.52}
 FILE=${2:-httpd.zip}
 
 
@@ -17,7 +17,23 @@ REPO_URL="http://devops-prod01.lab.sensus.net:8081/artifactory/csap-release/"
 #REPO_ID="csap-snapshot-repo"
 #REPO_URL="http://moc-artifactory.lab.sensus.net/artifactory/csap-snapshots/"
 
-mavenSettingsFile="$STAGING/conf/propertyOverride/settings.xml"
+mavenSettingsFile="$csapDefinitionResources/settings.xml"
+
+
+#
+# Windows bash shell on eclipse settings
+#
+
+windowsBashFolder="/mnt/d/downloads" ;
+#if test -d "/d/downloads" ; then
+#	windowsBashFolder="/d/downloads" ;
+#fi ;
+if test -d $windowsBashFolder ; then
+	pathToJavaBinary=${2:-$windowsBashFolder/$(basename $pathToJavaBinary)} ;
+	mavenSettingsFile="/mnt/c/Users/peter.nightingale/.m2/settings.xml"
+	echo "Found windowsBashFolder: '$windowsBashFolder' mavenSettingsFile: '$mavenSettingsFile'"
+fi
+
 
 mvn -s $mavenSettingsFile deploy:deploy-file \
   -DgroupId=$GROUP_ID \
--- csap-package-java-secondary/externalDeploy.sh
+++ csap-package-java-secondary/externalDeploy.sh
@@ -12,15 +12,18 @@ source ~/.bashrc
 
 
 
-wget https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_11.0.12_7.tar.gz
+#wget https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_11.0.12_7.tar.gz
+
+if test -d /mnt/CSAP_DEV01_NFS/csap-web-server/java; then
+	wget https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_11.0.13_8.tar.gz
+fi ;
 #
 #  linux settings
 #
 mavenSettingsFile="$csapDefinitionResources/settings.xml"
-mavenArtifactVersion=${1:-11.0.12_7} ;
+mavenArtifactVersion=${1:-11.0.13_8} ;
 pathToJavaBinary=${2:-/mnt/CSAP_DEV01_NFS/csap-web-server/java/OpenJDK11U-jdk_x64_linux_$mavenArtifactVersion.tar.gz} ;
-# mavenArtifactVersion=${1:-11.0.11_9} ;
-# pathToJavaBinary=${2:-$HOME/OpenJDK11U-jdk_x64_linux_11.0.11_9.tar.gz} ;
+
 
 
 
@@ -29,9 +32,7 @@ pathToJavaBinary=${2:-/mnt/CSAP_DEV01_NFS/csap-web-server/java/OpenJDK11U-jdk_x6
 #
 
 windowsBashFolder="/mnt/d/downloads" ;
-#if test -d "/d/downloads" ; then
-#	windowsBashFolder="/d/downloads" ;
-#fi ;
+
 if test -d $windowsBashFolder ; then
 	pathToJavaBinary=${2:-$windowsBashFolder/$(basename $pathToJavaBinary)} ;
 	mavenSettingsFile="/mnt/c/Users/peter.nightingale/.m2/settings.xml"
