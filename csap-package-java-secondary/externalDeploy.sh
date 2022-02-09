dedcaae74d7ca28c3fadfaed92d3cfaa125412a3
peterdnight <p.eternightingale@gmail.com>
2022-02-09 09:02:30 -0500

    csap-master-merge 22.01

--- csap-package-java-secondary/externalDeploy.sh
+++ csap-package-java-secondary/externalDeploy.sh
@@ -11,20 +11,27 @@ source ~/.bashrc
 #
 
 
+function print_section() { 
+	echo -e  "\n* \n**\n*** \n**** \n*****  $* \n**** \n*** \n** \n*"
+}
 
 #wget https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_11.0.12_7.tar.gz
 
 if test -d /mnt/CSAP_DEV01_NFS/csap-web-server/java; then
 	wget https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_11.0.13_8.tar.gz
 fi ;
+
+
 #
 #  linux settings
 #
 mavenSettingsFile="$csapDefinitionResources/settings.xml"
-mavenArtifactVersion=${1:-11.0.13_8} ;
-pathToJavaBinary=${2:-/mnt/CSAP_DEV01_NFS/csap-web-server/java/OpenJDK11U-jdk_x64_linux_$mavenArtifactVersion.tar.gz} ;
+#mavenArtifactVersion=${1:-11.0.13_8} ;
+mavenArtifactVersion=${1:-17.0.2} ;
+#pathToJavaBinary=${2:-/mnt/CSAP_DEV01_NFS/csap-web-server/java/OpenJDK11U-jdk_x64_linux_$mavenArtifactVersion.tar.gz} ;;
+pathToJavaBinary=${2:-/mnt/CSAP_DEV01_NFS/csap-web-server/java/openjdk-$mavenArtifactVersion\_linux-x64_bin.tar.gz} ;
 
-
+#openjdk-17.0.2_linux-x64_bin.tar.gz
 
 
 #
@@ -39,26 +46,31 @@ if test -d $windowsBashFolder ; then
 	echo "Found windowsBashFolder: '$windowsBashFolder' mavenSettingsFile: '$mavenSettingsFile'"
 fi
 
-echo "pathToJavaBinary: '$pathToJavaBinary' mavenArtifactVersion: '$mavenArtifactVersion'" ;
+print_section "pathToJavaBinary: '$pathToJavaBinary' mavenArtifactVersion: '$mavenArtifactVersion'" ;
 
-GROUP_ID="bin"
-ARTIFACT_ID="jdk"
-TYPE="tar.gz"
-REPO_ID="csap-release-repo"
-REPO_URL="http://devops-prod01.lab.sensus.net:8081/artifactory/csap-release/"
-#REPO_ID="csap-snapshot-repo"
-#REPO_URL="http://moc-artifactory.lab.sensus.net/artifactory/csap-snapshots/"
+if test -f $pathToJavaBinary ; then
 
-#exit ;
+	GROUP_ID="bin"
+	ARTIFACT_ID="jdk"
+	TYPE="tar.gz"
+	REPO_ID="csap-release-repo"
+	REPO_URL="http://devops-prod01.lab.sensus.net:8081/artifactory/csap-release/"
+	#REPO_ID="csap-snapshot-repo"
+	#REPO_URL="http://moc-artifactory.lab.sensus.net/artifactory/csap-snapshots/"
+	
+	#exit ;
+	
+	print_section "Uploading artifact $ARTIFACT_ID to maven: $REPO_URL"
+	
+	mvn --batch-mode --settings $mavenSettingsFile deploy:deploy-file \
+	  -DgroupId=$GROUP_ID \
+	  -DartifactId=$ARTIFACT_ID \
+	  -Dversion=$mavenArtifactVersion \
+	  -Dpackaging=$TYPE \
+	  -Dfile=$pathToJavaBinary \
+	  -DrepositoryId=$REPO_ID \
+	  -Durl=$REPO_URL
 
-
-
-mvn --batch-mode --settings $mavenSettingsFile deploy:deploy-file \
-  -DgroupId=$GROUP_ID \
-  -DartifactId=$ARTIFACT_ID \
-  -Dversion=$mavenArtifactVersion \
-  -Dpackaging=$TYPE \
-  -Dfile=$pathToJavaBinary \
-  -DrepositoryId=$REPO_ID \
-  -Durl=$REPO_URL
-
+else 
+	print_section "Verify binary location: $pathToJavaBinary"
+fi ;
